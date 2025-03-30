8868a36db9ef0249b4dd10799b8b84f436d31d27
緒方雄介 <ogatayuusuke@ogatayuusukenoMacBook-Pro.local>
2025-03-30 18:05:14 +0900

    add matchmaking

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router";
 import CreateAccount from "./components/createAccount/CreateAccount";
 import Home from "./components/home/Home";
 import Login from "./components/login/Login";
+import MatchMaking from "./components/matchMaking/MatchMaking";
 import { PAGE_PATH_URL } from "./utilities/const";
 
 const Router = () => {
@@ -10,6 +11,7 @@ const Router = () => {
     <Switch>
       <Route exact path={PAGE_PATH_URL.HOME} component={Home} />
       <Route exact path={PAGE_PATH_URL.LOGIN} component={Login} />
+      <Route exact path={PAGE_PATH_URL.MATCH_MAKING} component={MatchMaking} />
       <Route exact path={"/"} component={Login} />
       <Route exact path={PAGE_PATH_URL.CREATE_ACCOUNT} component={CreateAccount} />
     </Switch>
